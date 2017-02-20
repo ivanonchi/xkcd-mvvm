85f66e38965f427be53ac57a575ea557bc18998d
Ivan Luque <ivan.luque@glpgs.com>
2017-02-20 16:17:10 +0900

    Initial Commit

---
+++ xkcd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xkcd
+//
+//  Created by Ivan Luque on 2/20/17.
+//  Copyright © 2017 株式会社ガラパゴス. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
